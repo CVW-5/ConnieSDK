ae8f4b6549c532f9bac058b0ec7332d58b2ded78
Dani Lodholm <dlodholm@icloud.com>
2023-03-19 21:36:35 -0600

    Added ObjectType enum
    ObjectType provides some understanding of WHAT an object is and how it should be serialized
    Implemented test serialization

--- Assets/ConnieSDK/Code/ConnieSerializer.cs
+++ Assets/ConnieSDK/Code/ConnieSerializer.cs
@@ -1,22 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
+using System.Linq;
 using UnityEngine;
 
 #nullable enable
 namespace ConnieSDK
 {
+    public enum ObjectType
+    {
+        Generic = 0,
+        Unit = 1,
+        Store = 2,
+        MapData = 4,
+        Mission = 8,
+    }
+
     public static class ConnieSerializer
     {
         public static string AssetDirectory { get; private set; } =
             Path.Join(Environment.CurrentDirectory, "Assets");
-        public static Dictionary<string, string> FileTypes =
-            new Dictionary<string, string>
+        public static readonly Dictionary<ObjectType, string> FileTypes =
+            new Dictionary<ObjectType, string>
             {
-                {"Unit","zip" },
-                {"MapData", "zip" },
-                {"Stores","zip" },
+                {ObjectType.Generic,"zip" },
+                {ObjectType.Unit, "cwu" },
+                {ObjectType.Store,"cwu" },
+                {ObjectType.MapData,"cwmap" },
+                {ObjectType.Mission,"cwmis" },
             };
 
         public static void SetAssetDirectory(string path)
@@ -28,20 +39,50 @@ namespace ConnieSDK
         }
 
 #if UNITY_EDITOR
-        public static bool SerializeUnit(GameObject prefab, string outputName)
+        public static bool SerializeObject(GameObject prefab, ObjectType type, string outputName)
         {
-            string fullpath = Path.Join(AssetDirectory, $"{outputName}.{FileTypes["Unit"]}");
+            string fullpath = Path.Join(AssetDirectory, $"{outputName}.{FileTypes[type]}");
             Debug.Log($"Serializing a prefab to {fullpath}...");
 
-            using ArchiveWrapper archive = new ArchiveWrapper(fullpath, true);
+            using (ArchiveWrapper archive = new ArchiveWrapper(fullpath, true))
+            {
 
-            if (archive.HasEntry("EmptyAsset"))
-                archive.DeleteEntry("EmptyAsset");
+                if (archive.HasEntry("EmptyAsset"))
+                    archive.DeleteEntry("EmptyAsset");
+
+                DateTime generationTime = DateTime.UtcNow;
+
+                archive.WriteEntry("UnitData.txt",
+                    $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE\n\n" +
+                    $"ConnieSDK Unit Definition: General\n" +
+                    $"Name: {outputName}\n" +
+                    $"Type: {type}\n\n" +
+                    $"Generated at {generationTime:yyyymmdd HH:mm:ss} UTC\n" +
+                    $"Using ConnieSDK version {Globals.Version}\n\n" +
+                    $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE");
+                Debug.Log("Wrote Unit Data...");
+
+                archive.WriteEntry("Hierarchy.txt",
+                    $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE\n\n" +
+                    $"ConnieSDK Unit Definition: Hierarchy\n" +
+                    $"{prefab.name}\n - " +
+                    string.Join("\n - ", prefab.transform.GetChildren().Select(x => x.name)) + "\n" +
+                    $"Generated at {generationTime:yyyymmdd HH:mm:ss} UTC\n" +
+                    $"Using ConnieSDK version {Globals.Version}\n\n" +
+                    $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE");
+                Debug.Log("Wrote Hierarchy...");
+
+                Debug.Log($"Archive now includes {archive.EntryNames.Length} entries");
+            }
 
             Debug.Log("Done!");
             return true;
         }
 #endif
 
+        public static GameObject? DeserializeObject(string filepath, bool hideByDefault = true)
+        {
+            return null;
+        }
     }
 }
