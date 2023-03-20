08e58d733397881178f2190543a6ce179196f60f
Dani Lodholm <dlodholm@icloud.com>
2023-03-19 21:33:44 -0600

    Added global values class... for now

---
+++ Assets/ConnieSDK/Code/Globals.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnieSDK
+{
+    public static class Globals
+    {
+        public static readonly string Version = "a0.1.0";
+    }
+}
