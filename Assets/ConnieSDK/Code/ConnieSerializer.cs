33aebf0a357106a33d77c43ecc1d42a291484473
Dani Lodholm <dlodholm@icloud.com>
2023-03-21 21:04:02 -0600

    Update ConnieSerializer.cs
    Crawl the hierarchy to grab the object structure and serialize it to json

--- Assets/ConnieSDK/Code/ConnieSerializer.cs
+++ Assets/ConnieSDK/Code/ConnieSerializer.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using UnityEngine;
+using ConnieSDK.Components;
 
 #nullable enable
 namespace ConnieSDK
@@ -20,15 +23,22 @@ namespace ConnieSDK
     {
         public static string AssetDirectory { get; private set; } =
             Path.Join(Environment.CurrentDirectory, "Assets");
-        public static readonly Dictionary<ObjectType, string> FileTypes =
-            new Dictionary<ObjectType, string>
-            {
-                {ObjectType.Generic,"zip" },
-                {ObjectType.Unit, "cwu" },
-                {ObjectType.Store,"cwu" },
-                {ObjectType.MapData,"cwmap" },
-                {ObjectType.Mission,"cwmis" },
-            };
+        public static readonly Dictionary<ObjectType, string> FileTypes = new Dictionary<ObjectType, string>
+        {
+            {ObjectType.Generic,"zip" },
+            {ObjectType.Unit, "cwu" },
+            {ObjectType.Store,"cwu" },
+            {ObjectType.MapData,"cwmap" },
+            {ObjectType.Mission,"cwmis" },
+        };
+
+        public static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            WriteIndented = true,
+            NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            IncludeFields = true,
+        };
 
         public static void SetAssetDirectory(string path)
         {
@@ -44,6 +54,9 @@ namespace ConnieSDK
             string fullpath = Path.Join(AssetDirectory, $"{outputName}.{FileTypes[type]}");
             Debug.Log($"Serializing a prefab to {fullpath}...");
 
+            TransformWrapper hierarchy = new TransformWrapper(prefab.transform, 5, true, true);
+            Debug.Log($"Hierarchy crawled, found {hierarchy.Components.Length} valid components and {hierarchy.Children.Length} direct children");
+            
             using (ArchiveWrapper archive = new ArchiveWrapper(fullpath, true))
             {
 
@@ -62,14 +75,19 @@ namespace ConnieSDK
                     $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE");
                 Debug.Log("Wrote Unit Data...");
 
+                string hierarchyJson = JsonSerializer.Serialize(hierarchy, jsonOptions);
+
+                archive.WriteEntry("Hierarchy.json", hierarchyJson);
+                /*
                 archive.WriteEntry("Hierarchy.txt",
                     $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE\n\n" +
                     $"ConnieSDK Unit Definition: Hierarchy\n" +
                     $"{prefab.name}\n - " +
-                    string.Join("\n - ", prefab.transform.GetChildren().Select(x => x.name)) + "\n" +
+                    string.Join("\n - ", hierarchy.Children.Select(x => x.Name)) + "\n" +
                     $"Generated at {generationTime:yyyymmdd HH:mm:ss} UTC\n" +
                     $"Using ConnieSDK version {Globals.Version}\n\n" +
                     $"WARNING: THIS FILE FORMAT IS TEMPORARY, DO NOT USE IT FOR LONG TERM DATA STORAGE");
+                */
                 Debug.Log("Wrote Hierarchy...");
 
                 Debug.Log($"Archive now includes {archive.EntryNames.Length} entries");
