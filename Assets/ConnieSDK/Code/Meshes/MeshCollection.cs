21d2b01ae95518faa8122dbdc4254400355f8e45
Dani Lodholm <dlodholm@icloud.com>
2023-03-26 11:42:21 -0600

    Add MeshLibrary and MeshCollection classes

---
+++ Assets/ConnieSDK/Code/Meshes/MeshCollection.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnieSDK
+{
+    public class MeshCollection
+    {
+        private Dictionary<string, Mesh> meshes = new Dictionary<string, Mesh>();
+
+        public Mesh this[string name]
+        {
+            get => meshes[name];
+            set => meshes[name] = value;
+        }
+    }
+}
---
+++ Assets/ConnieSDK/Code/Meshes/MeshLibrary.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace ConnieSDK
+{
+    public static class MeshLibrary
+    {
+        private static Dictionary<string, MeshCollection> collections = new Dictionary<string, MeshCollection>();
+
+        public static MeshCollection AddCollection (string name)
+        {
+            if (collections.ContainsKey(name))
+                throw new Exception($"The mesh collection {name} already exists!");
+
+            MeshCollection mc = new MeshCollection();
+            collections[name] = mc;
+
+            return mc;
+        }
+
+        public static MeshCollection GetCollection (string name)
+        {
+            return collections[name];
+        }
+
+        public static Mesh GetMesh (string collection, string name)
+        {
+            if (collections[collection] is MeshCollection mc)
+                if (mc[name] is Mesh mesh)
+                    return mesh;
+
+            throw new IndexOutOfRangeException();
+        }
+
+        public static void SetMesh (string collection, string name, Mesh mesh)
+        {
+            if (collections[collection] is MeshCollection mc)
+                mc[name] = mesh;
+        }
+    }
+}
