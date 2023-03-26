5043312c49aa5c9244fb67d1d05692202a5867ec
Dani Lodholm <dlodholm@icloud.com>
2023-03-26 16:05:04 -0600

    Broke UnitDefinition into its own class because Unity is fussy about instantiating MonoBehaviors

--- Assets/ConnieSDK/Code/UnitData.cs
+++ Assets/ConnieSDK/Code/UnitData.cs
@@ -10,34 +10,14 @@ namespace ConnieSDK
 {
     public class UnitData : MonoBehaviour
     {
-        public string UnitName = string.Empty;
-        public ObjectType Type = ObjectType.Generic;
-
-        public string DescriptionShort = string.Empty;
-        [TextArea(minLines:5, maxLines:10)]
-        public string DescriptionLong = string.Empty;
-
-        public MeshWriteMode MeshWriting = MeshWriteMode.Nothing;
-        public string MeshCollection = string.Empty;
-
-        [JsonIgnore]
-        public string FileName = "newAsset";
-        [JsonIgnore]
-        public string[] IgnoreObjects = new string[0];
+        public UnitDefinition Definition = new UnitDefinition();
 
         [ContextMenu("Serialize to File")]
         public void Serialize()
         {
-            ConnieSerializer.SerializeObject(this);
-        }
-
-        [Flags]
-        public enum MeshWriteMode
-        {
-            Nothing = 0,
-            Write = 1,
-            Bundle = 2,
+            ConnieSerializer.SerializeObject(Definition, transform);
         }
     }
+
 }
 #endif
